7f191a1155faff6a7506dcd685ff6c9df7309e0f
zyb张一博 <yibozhang@ctrip.com>
2018-07-11 14:11:32 +0800

    new

--- ConsoleAppCancel/Program.cs
+++ ConsoleAppCancel/Program.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppCancel
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(RemoveDuplicates(new int[] { 1, 1, 2, 2, 3, 3 }));
 			JSchemaGenerator generator = new JSchemaGenerator();
 			generator.DefaultRequired = Required.DisallowNull;
 
@@ -34,6 +35,24 @@ namespace ConsoleAppCancel
 			Console.ReadLine();
 			Console.WriteLine("Hello World!");
 
-        }
-    }
+
+		}
+		public static int RemoveDuplicates(int[] nums)
+		{
+			if (nums.Length == 0)
+			{
+				return 0;
+			}
+			int number = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] != nums[number])
+				{
+					number++;
+					nums[number] = nums[i];
+				}
+			}
+			return ++number;
+		}
+	}
 }
---
+++ LearnNetCore/Formatter/ProtobufFormatter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
+using ProtoBuf;
+
+namespace LearnNetCore.Formatter
+{
+    public class ProtobufFormatter:OutputFormatter
+    {
+		public string ContentType { get; private set; }
+		public ProtobufFormatter()
+		{
+			ContentType = "application/proto";
+			SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/proto"));
+		}
+		public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+		{
+			if(context == null)
+			{
+				throw new ArgumentException(nameof(context));
+			}
+			var response = context.HttpContext.Response;
+			Serializer.Serialize(response.Body, context.Object);
+			return Task.FromResult(0);
+		}
+	}
+}
